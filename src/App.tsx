a8c1cbd13d5fce9ea60900187c66b0416222bcde
Vimal Raj <145170317+writerfx@users.noreply.github.com>
2026-04-27 01:46:08 +0530

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import Layout from './components/Layout';
 import Home from './pages/Home';
 import Decide from './pages/Decide';
 import Arrive from './pages/Arrive';
@@ -11,19 +12,29 @@ import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 import './App.css';
 
+function LayoutWrapper() {
+  return (
+    <Layout>
+      <Outlet />
+    </Layout>
+  );
+}
+
 function App() {
   return (
     <HelmetProvider>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/decide" element={<Decide />} />
-        <Route path="/arrive" element={<Arrive />} />
-        <Route path="/settle" element={<Settle />} />
-        <Route path="/packages" element={<Packages />} />
-        <Route path="/how-it-works" element={<HowItWorks />} />
-        <Route path="/legal" element={<Legal />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
+        <Route element={<LayoutWrapper />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/decide" element={<Decide />} />
+          <Route path="/arrive" element={<Arrive />} />
+          <Route path="/settle" element={<Settle />} />
+          <Route path="/packages" element={<Packages />} />
+          <Route path="/how-it-works" element={<HowItWorks />} />
+          <Route path="/legal" element={<Legal />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </HelmetProvider>
   );
